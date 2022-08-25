7ce97bab6fc74071c83b8eb6d2fe82cf153c612b
bb3800 <brianandrewbunn@gmail.com>
2022-08-25 20:35:54 +0000

    W3

---
+++ W3/app.js
@@ -0,0 +1,42 @@
+const showOnPage = function (text) {
+    let newParagraph = document.createElement("p")
+    newParagraph.innerHTML = text
+    let outputDiv = document.getElementById("output")
+    outputDiv.append(newParagraph)
+}
+
+showOnPage('<b>---Am I able to walk my dogs today?---</b>')
+
+
+let timeToWalkDog = function (timeWokeUp, twelvePm) {
+    let remainingTime = (twelvePm - timeWokeUp)
+   walkingPrompt = ''
+
+    if(remainingTime >= 1) {
+        walkingPrompt = 'is plenty of time left, you can still walk dogs'
+    } else if (remainingTime <1) {
+        walkingPrompt = 'isnt enough time, you cannot walk dog'
+    }
+   
+    return `you have ${remainingTime} hours left before noon. There ${walkingPrompt}`
+} 
+
+let result = timeToWalkDog(8,12)
+
+showOnPage(result)
+
+let whatIsTheTemp = function (ninteyFiveDegrees, currentTemp) {
+    let tooHot = (ninteyFiveDegrees - currentTemp)
+   tempPrompt = ''
+
+   if (tooHot < 95) {
+    tempPrompt = 'It is cool enough to walk dogs'
+   } else if (tooHot > 95) {
+    tempPrompt = 'It is too hot, you cannot walk dogs'
+   }
+
+   return `It is ${tooHot} degrees under 95, ${tempPrompt}`
+}
+let answer = whatIsTheTemp(95,80)
+
+showOnPage(answer)
