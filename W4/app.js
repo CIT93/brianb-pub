b2b15c414c381713973093a1b1517860903b35fc
bb3800 <brianandrewbunn@gmail.com>
2022-09-02 00:20:54 +0000

    W4

--- W4/app.js
+++ W4/app.js
@@ -1,36 +1,36 @@
 const showOnPage = function (text) {
-    let newParagraph = document.createElement("p")
-    newParagraph.innerHTML = text
-    let outputDiv = document.getElementById("output")
-    outputDiv.append(newParagraph)
-}
+  let newParagraph = document.createElement("p");
+  newParagraph.innerHTML = text;
+  let outputDiv = document.getElementById("output");
+  outputDiv.append(newParagraph);
+};
 
-showOnPage('<b>---Am I able to walk my dogs today?---</b>')
+showOnPage("<b>---Am I able to walk my dogs today?---</b>");
 
 const walkTheDogs = {
-    timeToWalkDog: function (remainingTime) {
-       walkingPrompt = ''
-    
-        if(remainingTime >= 1) {
-            walkingPrompt = 'is plenty of time left, you can still walk dogs'
-        } else if (remainingTime <1) {
-            walkingPrompt = 'isnt enough time, you cannot walk dog'
-        }
-       
-        return `you have ${remainingTime} hours left before noon. There ${walkingPrompt}`
-    },
-    whatIsTheTemp: function (tooHot) {
-       tempPrompt = ''
-    
-       if (tooHot < 95) {
-        tempPrompt = 'It is cool enough to walk dogs'
-       } else if (tooHot > 95) {
-        tempPrompt = 'It is too hot, you cannot walk dogs'
-       }
-    
-       return `It is ${tooHot} degrees under 95, ${tempPrompt}`
-    }
-}
+  timeToWalkDog: function (remainingTime) {
+    walkingPrompt = "";
 
-showOnPage (walkTheDogs.timeToWalkDog(1))
-showOnPage (walkTheDogs.whatIsTheTemp(85))
+    if (remainingTime >= 1) {
+      walkingPrompt = "is plenty of time left, you can still walk dogs";
+    } else if (remainingTime < 1) {
+      walkingPrompt = "isnt enough time, you cannot walk dog";
+    }
+
+    return `you have ${remainingTime} hours left before noon. There ${walkingPrompt}`;
+  },
+  whatIsTheTemp: function (tooHot) {
+    tempPrompt = "";
+
+    if (tooHot < 95) {
+      tempPrompt = "It is cool enough to walk dogs";
+    } else if (tooHot > 95) {
+      tempPrompt = "It is too hot, you cannot walk dogs";
+    }
+
+    return `It is ${tooHot} degrees under 95, ${tempPrompt}`;
+  },
+};
+
+showOnPage(walkTheDogs.timeToWalkDog(1));
+showOnPage(walkTheDogs.whatIsTheTemp(85));
