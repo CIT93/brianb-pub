629f9dd6c2af6398013cd9e754b9d9f194c441c9
bb3800 <brianandrewbunn@gmail.com>
2022-09-29 21:51:10 +0000

    W8

--- W7/app.js
+++ W7/app.js
@@ -1,44 +1,67 @@
-
+//array of objects
 
 const dayList = [
   {
-    day: 'Monday',
+    day: "Thursday",
+    temp: 90,
+    awake: 11,
   },
   {
-    day: 'Tuesday',
+    day: "Friday",
+    temp: 99,
+    awake: 12,
   },
   {
-    day: 'Wednesday',
+    day: "Saturday",
+    temp: 80,
+    awake: 10,
   },
-  {
-    day: 'Thursday'
-  }
 ];
 
-const dogWalk = function () { 
-    let temp = Math.floor(Math.random() * 120);
-    let timeWokeUp = Math.floor(Math.random() * 15);
-   
-    if (timeWokeUp >= 12 && temp >= 95) {
+//function factoring in the temp and time woke up from the array of objects
+let dogWalk = function (obj) {
+  if (obj.awake >= 12 && obj.temp >= 95) {
     return "It is too late and too hot to walk the dogs";
-  } else if (timeWokeUp >= 12 && temp < 95) {
+  } else if (obj.awake >= 12 && obj.temp < 95) {
     return "It is too late to walk the dogs";
-  } else if (timeWokeUp < 12 && temp >= 95) {
+  } else if (obj.awake < 12 && obj.temp >= 95) {
     return "It is too hot to walk the dogs";
-  } else if (timeWokeUp < 12 && temp < 95) {
-    return "It is still cool and early, you may walk your";
+  } else if (obj.awake < 12 && obj.temp < 95) {
+    return "It is still cool and early, you may walk your dogs";
   }
 };
 
-const decision = function () {
-  dayList.forEach(function () {
-  });
+//global variable linking dayInfo to the array & defining dayOff as true
+let dayInfo = [];
+let dayOff = true;
+
+//function rendering output to DOM and my attempt to plug in a function the checkbox that overrides other function
+const loop = function () {
+  if (dayOff === false) {
+    document.querySelector("#dayInfo").innerHTML =
+      "You have work today, you cannot walk the dogs";
+  } else {
+    dayList.forEach(function (obj) {
+      dayInfo.push(dogWalk(obj));
+    });
+  }
+  return dayInfo;
 };
 
-decision();
+//event listener that shows the submit button has been clicked
+document.querySelector("button").addEventListener("click", function (e) {
+  document.getElementById("dayInfo").innerHTML = loop();
+  e.target.textContent = "Results are being displayed";
+});
 
-document.querySelector('button').addEventListener('click', function(e) {
-    const newParagraph = document.createElement('p')
-newParagraph.textContent = (dogWalk)
-document.querySelector('body').appendChild(newParagraph)
-})
+//event listener that monitors whether the checkbox was checked or not
+document.querySelector("#checkbox").addEventListener("change", function (e) {
+  dayOff = e.target.checked;
+});
+
+//event listener that allows the remove results button to remove results
+document.querySelector("#remove-all").addEventListener("click", function () {
+  document.querySelectorAll("#dayInfo").forEach(function (result) {
+    result.remove();
+  });
+});
